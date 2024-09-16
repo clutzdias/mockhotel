8341aaf525fbea4223905d276cb99b2e2d6c15de
Carla Lutz <clutzdias@gmail.com>
2024-09-16 20:15:32 -0300

    Inicio da criacao da camada de views

---
+++ urls.py
@@ -0,0 +1,25 @@
+from flask import BluePrint, request
+
+from views.reservas_view import ReservasView
+
+blue_print = BluePrint('main', __name__)
+
+@blue_print.route('/reservas', methods=['GET', 'POST'])
+def reservas():
+  if request.method == 'POST':
+    return ReservasView().post()
+  else:
+    return ReservasView().get()
+
+@blue_print.route('/reservas/<id_reserva>', methods=['PATCH', 'DELETE'])
+def reservas_id(id_reserva):
+  if request.method == 'PATCH':
+    return ReservasView().patch(id_reserva)
+  else:
+    return ReservasView().cancelar(id_reserva)
+  
+def register_routes(app):
+  app.register_blueprint(blue_print)
+
+
+
---
+++ views/reservas_view.py
@@ -0,0 +1,15 @@
+from flask import request
+
+class ReservasView:
+  
+  def post():
+    pass
+
+  def get():
+    pass
+  
+  def patch(id_reserva):
+    pass
+
+  def cancelar(id_reserva):
+    pass
